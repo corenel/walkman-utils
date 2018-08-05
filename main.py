ba9bf611f5a5a0c1bf170a861988729b66c95433
corenel <xxdsox@gmail.com>
2018-08-05 21:53:27 +0800

    implement file updating and removing

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
+import os
 import util
+import shutil
+
+from tqdm import tqdm
 
 
-def sync_playlist(playlists, target_dir):
+def sync_playlist(playlists, target_dir, remove_unmatched=False):
+    # Get music list
     files_in_playlists = util.get_files_in_playlist(playlists)
     itunes_folder, files_in_itunes = util.split_filepath(files_in_playlists)
     files_on_walkman = util.scan_directory(target_dir)
+    to_be_updated, to_be_removed, to_be_ignored = util.compare_filelists(
+        files_in_playlists, files_on_walkman,
+        root_src=itunes_folder, root_dst=target_dir
+    )
+
+    # Update music
+    progress = tqdm(sorted(to_be_updated))
+    for f in progress:
+        progress.set_description('Updating {}'.format(os.path.basename(f)))
+        target_path = os.path.join(target_dir, os.path.dirname(f))
+        if not os.path.exists(target_path):
+            os.makedirs(target_path)
+        shutil.copy2(os.path.join(itunes_folder, f), target_path)
+
+    if remove_unmatched:
+        progress = tqdm(sorted(to_be_removed))
+        for f in progress:
+            if not util.is_extension(f, 'lrc'):
+                progress.set_description('Removing {}'.format(os.path.basename(f)))
+                target_path = os.path.join(target_dir, os.path.dirname(f))
+                os.remove(target_path)
 
 
 if __name__ == '__main__':
--- util.py
+++ util.py
@@ -52,7 +52,7 @@ def scan_directory(target):
     """
     Scan given directory and get file lists
 
-    :param target: targte directory
+    :param target: target directory
     :type target: str
     :return: relative file lists
     :rtype: list[str]
@@ -67,3 +67,50 @@ def scan_directory(target):
             relative_path.append(os.path.join(curr, f))
 
     return relative_path
+
+
+def compare_filelists(files_src, files_dst, root_src, root_dst):
+    """
+    Compare two file lists
+
+    :param files_src: list of files in source directory
+    :type files_src: list[str]
+    :param files_dst: list of files in source directory
+    :type files_dst: list[str]
+    :param root_src: path to source directory
+    :type root_src: str
+    :param root_dst: path to source directory
+    :type root_dst: str
+    :return: list of files to be updated, removed and ignored
+    :rtype: tuple(list, list, list)
+    """
+    to_be_updated = [x for x in files_src if x not in files_dst]
+    to_be_removed = [x for x in files_dst if x not in files_src]
+    to_be_ignored = []
+
+    files_on_both_sides = [x for x in files_src if x in files_dst]
+    for f in files_on_both_sides:
+        if os.path.getmtime(os.path.join(root_src, f)) > \
+                os.path.getmtime(os.path.join(root_dst, f)):
+            to_be_updated.append(f)
+        else:
+            to_be_ignored.append(f)
+
+    return to_be_updated, to_be_removed, to_be_ignored
+
+
+def is_extension(filepath, ext):
+    """
+    Check whether file extension is desired
+
+    :param filepath: path to file
+    :type filepath: str
+    :param ext: valid extension
+    :type ext: str or list
+    :return: whether file extension is desired
+    :rtype: bool
+    """
+    if isinstance(ext, str):
+        ext = [ext]
+    filename, fileext = os.path.splitext(filepath)
+    return fileext in ext
